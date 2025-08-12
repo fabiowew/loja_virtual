a7ac255dff3487e070076a09f28044db51fb3b05
Home <Home@DESKTOP-J7MGQOI.mshome.net>
2025-08-12 13:28:15 -0300

    Comit inicial projeto java

---
+++ loja_virtual/src/main/java/dev/loja/virtual/LojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package dev.loja.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+}
