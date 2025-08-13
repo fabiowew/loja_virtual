be97050c52d620b0464a9533aff28f1f7febae1c
Home <Home@DESKTOP-J7MGQOI.mshome.net>
2025-08-12 22:27:53 -0300

    2 commit apos configurar e acertar a criacao de tabelas da base de dados!

--- loja_virtual/src/main/java/dev/loja/virtual/LojaVirtualApplication.java
+++ loja_virtual/src/main/java/dev/loja/virtual/LojaVirtualApplication.java
@@ -2,8 +2,10 @@ package dev.loja.virtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "dev.lojavirtual.model") 
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
